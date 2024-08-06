31c7fabc071b5d125827cb7f3ade60bb53015f28
esdras marcelino <esdrasmarcelino55@gmail.com>
2024-08-06 15:10:56 -0300

    feat: add components header and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
 "use client"
 
+import { Search } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
   return (
     <>
-      <h1>Home page</h1>
-      <Button>Teste</Button>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Esdras! </h2>
+        <p>Terça-feira, 06 de Agosto.</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Search" className="rounded-xl" />
+          <Button className="text-{white} rounded-xl bg-violet-400">
+            <Search />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FWS Barber "
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </>
   )
 }
