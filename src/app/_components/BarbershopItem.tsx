fe289172a5ae22e6835a2c7b4f62c266a92d19d8
esdras marcelino <esdrasmarcelino55@gmail.com>
2024-08-07 16:32:56 -0300

    feat: component BarbershopPage add

--- src/app/_components/BarbershopItem.tsx
+++ src/app/_components/BarbershopItem.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { Button } from "./ui/button"
 import { Badge } from "./ui/badge"
 import { StarIcon } from "lucide-react"
+import Link from "next/link"
 
 interface BarbershopItemProps {
   barbershop: Barbershop
@@ -25,7 +26,7 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
             className="absolute left-2 top-2 space-x-1"
             variant="secondary"
           >
-            <StarIcon size={12} fill="#7F61FC" className="text-violet-400" />
+            <StarIcon size={12} fill="#7F61FC" className="text-[#7F61FC]" />
             <p className="text-xs font-semibold">5,0</p>
           </Badge>
         </div>
@@ -33,8 +34,12 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
         <div className="px-1 py-3">
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
           <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
-          <Button variant="secondary" className="mt-3 w-full rounded-xl">
-            Reservar
+          <Button
+            variant="secondary"
+            className="mt-3 w-full rounded-xl"
+            asChild
+          >
+            <Link href={`/barbershops/${barbershop.id}`}>Reservar</Link>
           </Button>
         </div>
       </CardContent>
